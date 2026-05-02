565a33d4b110ffe54fefe9657195e01369c7c4e0
Jill <denissebauts@gmail.com>
2026-05-01 20:01:08 -0700

    database connection fix

--- databaseConnection.js
+++ databaseConnection.js
@@ -5,6 +5,17 @@ const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
 
 const MongoClient = require("mongodb").MongoClient;
-const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/`;
+const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
 var database = new MongoClient(atlasURI, {});
+async function connectDB() {
+  try {
+    await database.connect();
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("DB connection error:", err);
+  }
+}
+
+connectDB();
+
 module.exports = { database };
