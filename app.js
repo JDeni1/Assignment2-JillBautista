a7e36c034c225a3fdca70615de4b3be7d737339a
Jill <denissebauts@gmail.com>
2026-05-02 10:38:24 -0700

    debugging sessions database

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
 const mongodb_user_database = process.env.MONGODB_USER_DATABASE;
 const mongodb_session_database = process.env.MONGODB_SESSION_DATABASE;
+console.log("Session DB:", mongodb_session_database);
 const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
 const node_session_secret = process.env.NODE_SESSION_SECRET;
 
