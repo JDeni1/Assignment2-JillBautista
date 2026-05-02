3136301c62eee6e091cc4c38af64018f79b1b18c
Jill <denissebauts@gmail.com>
2026-05-02 12:49:45 -0700

    Added explicit ttl settings to ensure encryption

--- app.js
+++ app.js
@@ -41,7 +41,9 @@ var mongoStore = MongoStore.create({
   mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/`,
   dbName: mongodb_session_database,
   collectionName: "sessions",
-  autoRemove: "disabled",
+  ttl: 60 * 60,
+  autoRemove: "interval",
+  autoRemoveInterval: 10,
   crypto: {
     secret: mongodb_session_secret,
   },
