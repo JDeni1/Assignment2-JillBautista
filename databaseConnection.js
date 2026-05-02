0068281fdde25da04c7dad8da3c523416322f284
Jill <denissebauts@gmail.com>
2026-05-01 21:38:18 -0700

    troubleshooting atlasURI

--- databaseConnection.js
+++ databaseConnection.js
@@ -5,8 +5,7 @@ const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
 
 const MongoClient = require("mongodb").MongoClient;
-const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
-
+const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/`;
 console.log(atlasURI);
 
 var database = new MongoClient(atlasURI, {});
