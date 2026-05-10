02115bb97904cd5893d966fb6f870d217a44d072
Jill <denissebauts@gmail.com>
2026-05-10 15:47:33 -0700

    redirects admin to home

--- app.js
+++ app.js
@@ -206,7 +206,7 @@ app.get("/members", (req, res) => {
 
 // Admin
 app.get("/admin", async (req, res) => {
-  if (!req.session.name) return res.redirect("/login");
+  if (!req.session.name) return res.redirect("/");
   if (req.session.user_type !== "admin") {
     return res.status(403).render("403", { 
       navItems: navLinks,
